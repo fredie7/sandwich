70e92545f826669d07bb01605928bd98761fc56e
fredie <lexfredeick3@gmail.com>
2020-03-28 15:39:45 +0100

    project setup

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Layout = props => {
+    return (
+        <div>
+            <main>
+                {props.children}
+            </main>
+        </div>
+    )
+}
+export default Layout
